a38b4929717a40f9cd491733b85df5a1d8552a1d
ehorlacher <90493176+ehorlacher@users.noreply.github.com>
2022-02-16 19:17:38 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using BookStoreM7.Models;
+using BookStoreM7.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreM7.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookStoreRepository repo;
+        public HomeController (IBookStoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 10;
+
+            var x = new BooksViewModel
+            {
+                Books = repo.Books
+                .OrderBy(t => t.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+
+            return View(x);
+        }
+
+    }
+}
---
+++ Models/IBookStoreRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreM7.Models
+{
+    public interface IBookStoreRepository
+    {
+        IQueryable<Book> Books { get; }
+    }
+}
